ac7c8d823b976a31a2541b5c9f07e3a76da46cf4
Human-developer <liouvnet0705@naver.com>
2022-11-07 10:08:20 +0900

    10866 구현풀이

---
+++ 0x07_덱/10866_덱_구현풀이.cpp
@@ -0,0 +1,194 @@
+/*
+
+백준 10866번
+
+덱
+
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+0.5 초 (추가 시간 없음)	256 MB	53628	29412	24883	56.179%
+
+문제
+정수를 저장하는 덱(Deque)를 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
+
+명령은 총 여덟 가지이다.
+
+push_front X: 정수 X를 덱의 앞에 넣는다.
+push_back X: 정수 X를 덱의 뒤에 넣는다.
+pop_front: 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+pop_back: 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+size: 덱에 들어있는 정수의 개수를 출력한다.
+empty: 덱이 비어있으면 1을, 아니면 0을 출력한다.
+front: 덱의 가장 앞에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+back: 덱의 가장 뒤에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+입력
+첫째 줄에 주어지는 명령의 수 N (1 ≤ N ≤ 10,000)이 주어진다. 둘째 줄부터 N개의 줄에는 명령이 하나씩 주어진다. 
+주어지는 정수는 1보다 크거나 같고, 100,000보다 작거나 같다. 문제에 나와있지 않은 명령이 주어지는 경우는 없다.
+
+출력
+출력해야하는 명령이 주어질 때마다, 한 줄에 하나씩 출력한다.
+
+예제 입력 1 
+15
+push_back 1
+push_front 2
+front
+back
+size
+empty
+pop_front
+pop_back
+pop_front
+size
+empty
+pop_back
+push_front 3
+empty
+front
+
+예제 출력 1 
+2
+1
+2
+0
+2
+1
+-1
+0
+1
+-1
+0
+3
+
+
+예제 입력 2 
+22
+front
+back
+pop_front
+pop_back
+push_front 1
+front
+pop_back
+push_back 2
+back
+pop_front
+push_front 10
+push_front 333
+front
+back
+pop_back
+pop_back
+push_back 20
+push_back 1234
+front
+back
+pop_back
+pop_back
+
+예제 출력 2 
+-1
+-1
+-1
+-1
+1
+1
+2
+2
+333
+10
+10
+333
+20
+1234
+1234
+20
+ 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MX = 1000005;
+int dat[2*MX+1];
+int head = MX, tail = MX;
+
+void push_front(int x) {
+    dat[--head] = x;
+}
+void push_back(int x) {
+    dat[tail++] = x;
+}
+void pop_front() {
+    head++;
+}
+void pop_back() {
+    tail--;
+}
+int size() {
+    return tail-head;
+}
+int empty() {
+    return (int)(tail-head == 0);
+}
+int front() {
+    return dat[head];
+}
+int back() {
+    return dat[tail-1];
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    while(N--) {
+        string s;
+        cin >> s;
+        if (s == "push_front") {
+            int n1;
+            cin >> n1;
+            push_front(n1);
+        }else if (s == "push_back") {
+            int n2;
+            cin >> n2;
+            push_back(n2);
+        }else if (s == "pop_front") {
+            if (empty())
+                cout << -1 << '\n';
+            else {
+                cout << front() << '\n';
+                pop_front();
+            }
+        }else if (s == "pop_back") {
+            if (empty())
+                cout << -1 << '\n';
+            else {
+                cout << back() << '\n';
+                pop_back();
+            }
+        }else if (s == "size") {
+            cout << size() << '\n';
+        }else if (s == "empty") {
+            cout << empty() << '\n';
+        }else if (s == "front") {
+            if (empty())
+                cout << -1 << '\n';
+            else {
+                cout << front() << '\n';
+            }
+        }else{
+            if (empty())
+                cout << -1 << '\n';
+            else {
+                cout << back() << '\n';
+            }
+        }
+    }
+
+    return 0;
+}
